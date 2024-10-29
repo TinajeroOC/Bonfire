fc18f5d0491642239bdcb42d00019f5065a761b9
TinajeroOC <88462409+TinajeroOC@users.noreply.github.com>
2024-10-29 14:22:39 -0700

    Add GraphQL codegen

---
+++ web/codegen.ts
@@ -0,0 +1,45 @@
+import type { CodegenConfig } from "@graphql-codegen/cli"
+
+const config: CodegenConfig = {
+  overwrite: true,
+  schema: "http://localhost:4000",
+  documents: "src/graphql/operations/**/*.{gql,graphql}",
+  generates: {
+    "src/graphql/__generated__/types.d.ts": {
+      plugins: [
+        "typescript",
+        {
+          add: {
+            content: "/* eslint-disable */",
+          },
+        },
+      ],
+      config: {
+        enumsAsTypes: true,
+      },
+    },
+    "src/graphql/__generated__/operations.ts": {
+      plugins: [
+        {
+          add: {
+            content: "/* eslint-disable */",
+          },
+        },
+        "typescript",
+        "typescript-operations",
+        "typed-document-node",
+      ],
+      config: {
+        enumsAsTypes: true,
+      },
+    },
+    "src/graphql/__generated__/schema.graphql": {
+      plugins: ["schema-ast"],
+      config: {
+        includeDirectives: true,
+      },
+    },
+  },
+}
+
+export default config
