9a2b616e63480ba32dda2f844d99edb4b1235429
TinajeroOC <88462409+TinajeroOC@users.noreply.github.com>
2024-11-05 08:29:23 -0800

    Propogate Authorization header to subgraphs

--- gateway/src/index.ts
+++ gateway/src/index.ts
@@ -1,14 +1,33 @@
 import { ApolloServer } from "apollo-server"
-import { ApolloGateway, IntrospectAndCompose } from "@apollo/gateway"
+import {
+  ApolloGateway,
+  IntrospectAndCompose,
+  RemoteGraphQLDataSource,
+  GraphQLDataSourceProcessOptions,
+} from "@apollo/gateway"
+
+class AuthenticatedDataSource extends RemoteGraphQLDataSource {
+  async willSendRequest(options: GraphQLDataSourceProcessOptions<Record<string, any>>) {
+    if (options.request.http && options.context.authorization) {
+      options.request.http.headers.set("Authorization", options.context.authorization)
+    }
+  }
+}
 
 const gateway = new ApolloGateway({
   supergraphSdl: new IntrospectAndCompose({
     subgraphs: [{ name: "AccountService", url: "http://127.0.0.1:8000/graphql/" }],
   }),
+  buildService({ name, url }) {
+    return new AuthenticatedDataSource({ url })
+  },
 })
 
 const server = new ApolloServer({
   gateway,
+  context: ({ req }) => ({
+    authorization: req.headers.authorization,
+  }),
 })
 
 server.listen(4000).then(({ url }) => {
