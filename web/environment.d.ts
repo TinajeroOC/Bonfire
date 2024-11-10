a2f52208856d3c8c118c3ef853ba8b4e3c2a9767
TinajeroOC <88462409+TinajeroOC@users.noreply.github.com>
2024-11-09 19:10:43 -0800

    Add NextAuth variables to ProcessEnv

--- web/environment.d.ts
+++ web/environment.d.ts
@@ -3,6 +3,8 @@ declare global {
     interface ProcessEnv {
       [key: string]: string | undefined
       NEXT_PUBLIC_APOLLO_SERVER_URI: string
+      NEXTAUTH_URL: string
+      NEXTAUTH_SECRET: string
     }
   }
 }
