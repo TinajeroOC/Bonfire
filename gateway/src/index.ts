beb5cb965402617d2515fd3d6280dd4330366f28
TinajeroOC <88462409+TinajeroOC@users.noreply.github.com>
2024-11-09 19:09:20 -0800

    Add multipart file upload support to Apollo Gateway

--- gateway/src/index.ts
+++ gateway/src/index.ts
@@ -1,35 +1,73 @@
-import { ApolloServer } from "apollo-server"
-import {
-  ApolloGateway,
-  IntrospectAndCompose,
-  RemoteGraphQLDataSource,
-  GraphQLDataSourceProcessOptions,
-} from "@apollo/gateway"
+import { ApolloServer } from "@apollo/server"
+import { ApolloGateway, IntrospectAndCompose, GraphQLDataSourceProcessOptions } from "@apollo/gateway"
+import { expressMiddleware } from "@apollo/server/express4"
+import FileUploadDataSource from "@abrevior/apollo-federation-upload"
+import express from "express"
+import cors from "cors"
+import { graphqlUploadExpress } from "graphql-upload-ts"
 
-class AuthenticatedDataSource extends RemoteGraphQLDataSource {
+class AuthenticatedDataSource extends FileUploadDataSource {
   async willSendRequest(options: GraphQLDataSourceProcessOptions<Record<string, any>>) {
-    if (options.request.http && options.context.authorization) {
-      options.request.http.headers.set("Authorization", options.context.authorization)
+    if (!options.request) {
+      options.request = {}
+    }
+
+    if (!options.request.http) {
+      options.request.http = {
+        method: "POST",
+        url: this.url,
+        headers: new Headers(),
+      }
+    }
+
+    const authorizationHeader = options.context.req?.headers?.authorization || options.context.authorization
+
+    if (authorizationHeader) {
+      options.request.http.headers.set("Authorization", authorizationHeader)
     }
   }
 }
 
-const gateway = new ApolloGateway({
-  supergraphSdl: new IntrospectAndCompose({
-    subgraphs: [{ name: "AccountService", url: "http://127.0.0.1:8000/graphql/" }],
-  }),
-  buildService({ name, url }) {
-    return new AuthenticatedDataSource({ url })
-  },
-})
+async function startApolloServer() {
+  const app = express()
 
-const server = new ApolloServer({
-  gateway,
-  context: ({ req }) => ({
-    authorization: req.headers.authorization,
-  }),
-})
+  const gateway = new ApolloGateway({
+    supergraphSdl: new IntrospectAndCompose({
+      subgraphs: [{ name: "AccountService", url: "http://127.0.0.1:8000/graphql/" }],
+    }),
+    buildService: ({ url }) =>
+      new AuthenticatedDataSource({
+        url,
+        useChunkedTransfer: false,
+      }),
+  })
 
-server.listen(4000).then(({ url }) => {
-  console.log(`Apollo Server ready at ${url}`)
-})
+  const server = new ApolloServer({
+    gateway,
+    csrfPrevention: true,
+  })
+
+  await server.start()
+
+  app.use(
+    "/graphql",
+    cors<cors.CorsRequest>(),
+    express.json(),
+    graphqlUploadExpress({
+      maxFileSize: 10000000,
+      maxFiles: 3,
+    }),
+    expressMiddleware(server, {
+      context: async ({ req }) => {
+        return {
+          req,
+          authorization: req.headers.authorization,
+        }
+      },
+    })
+  )
+
+  app.listen(4000)
+}
+
+startApolloServer()
