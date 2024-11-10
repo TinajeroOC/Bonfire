ccc1cfa938c0ef1248b3c08b11beef548e316aeb
TinajeroOC <88462409+TinajeroOC@users.noreply.github.com>
2024-11-10 10:41:42 -0800

    Add refresh token config to Account Service apps

--- services/AccountService/app/settings.py
+++ services/AccountService/app/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'graphql_jwt.refresh_token.apps.RefreshTokenConfig',
     'graphene_django',
     'service',
 ]
