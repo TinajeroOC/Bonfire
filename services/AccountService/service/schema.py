9bd886abfb2ef9a386f22a94fc7fd35713dc9486
TinajeroOC <88462409+TinajeroOC@users.noreply.github.com>
2024-11-10 20:02:09 -0800

    Rename and merge existing Account Service mutations

--- services/AccountService/service/schema.py
+++ services/AccountService/service/schema.py
@@ -14,15 +14,30 @@ class UserType(DjangoObjectType):
         model = User
         exclude = ['password', 'first_name', 'last_name']
 
-    profile_picture_url = graphene.String()
+    avatar_url = graphene.String()
+    banner_url = graphene.String()
 
-    def resolve_profile_picture_url(self, info):
-        if self.profile_picture:
-            return info.context.build_absolute_uri(self.profile_picture.url)
+    def resolve_avatar_url(self, info):
+        if self.avatar:
+            return info.context.build_absolute_uri(self.avatar.url)
+        return None
+
+    def resolve_banner_url(self, info):
+        if self.banner:
+            return info.context.build_absolute_uri(self.banner.url)
         return None
 
 
-class CreateUser(graphene.Mutation):
+class Auth(graphql_jwt.JSONWebTokenMutation):
+    refresh_token = graphene.NonNull(graphene.String)
+    user = graphene.NonNull(UserType)
+
+    @classmethod
+    def resolve(cls, root, info, **kwargs):
+        return cls(user=info.context.user, refresh_token=create_refresh_token(info.context.user))
+
+
+class CreateAccount(graphene.Mutation):
     success = graphene.Boolean(required=True)
     message = graphene.String()
     user = graphene.Field(UserType)
@@ -36,10 +51,10 @@ class CreateUser(graphene.Mutation):
 
     def mutate(self, info, username, email, password):
         if User.objects.filter(username=username).exists():
-            return CreateUser(success=False, message='An account with that username already exists.')
+            return CreateAccount(success=False, message='An account with that username already exists.')
 
         if User.objects.filter(email=email).exists():
-            return CreateUser(success=False, message='An account with that email already exists.')
+            return CreateAccount(success=False, message='An account with that email already exists.')
 
         user = User(
             username=username,
@@ -51,10 +66,93 @@ class CreateUser(graphene.Mutation):
         token = get_token(user)
         refresh_token = create_refresh_token(user)
 
-        return CreateUser(success=True, message='Account created.', user=user, token=token, refresh_token=refresh_token)
+        return CreateAccount(success=True, message='Account created.', user=user, token=token, refresh_token=refresh_token)
 
 
-class UpdatePassword(graphene.Mutation):
+class UpdateAccountProfile(graphene.Mutation):
+    success = graphene.Boolean(required=True)
+    message = graphene.String()
+    user = graphene.Field(UserType)
+
+    class Arguments:
+        email = graphene.String()
+        display_name = graphene.String()
+        description = graphene.String()
+
+    @login_required
+    def mutate(self, info, **kwargs):
+        user = info.context.user
+
+        if 'email' in kwargs and kwargs['email'] != user.email:
+            if User.objects.filter(email=kwargs['email']).exists():
+                return UpdateAccountProfile(
+                    success=False,
+                    message='An account with that email already exists.'
+                )
+
+        for field, value in kwargs.items():
+            setattr(user, field, value)
+
+        user.save()
+
+        return UpdateAccountProfile(
+            success=True,
+            message='Account updated.',
+            user=user
+        )
+
+
+class UpdateAccountMedia(graphene.Mutation):
+    success = graphene.Boolean(required=True)
+    message = graphene.String()
+    avatar_url = graphene.String()
+    banner_url = graphene.String()
+
+    class Arguments:
+        avatar = Upload()
+        banner = Upload()
+
+    @login_required
+    def mutate(self, info, **kwargs):
+        user = info.context.user
+        allowed_extensions = ['.jpg', '.jpeg', '.png']
+
+        avatar_url = None
+        banner_url = None
+
+        if 'avatar' in kwargs:
+            avatar = kwargs['avatar']
+            if not any(avatar['name'].lower().endswith(ext) for ext in allowed_extensions):
+                return UpdateAccountMedia(
+                    success=False,
+                    message="User avatar must be a PNG or JPEG file."
+                )
+            user.avatar = avatar['promise']
+            avatar_url = info.context.build_absolute_uri(
+                user.profile_picture.url)
+
+        if 'banner' in kwargs:
+            banner = kwargs['banner']
+            if not any(banner['name'].lower().endswith(ext) for ext in allowed_extensions):
+                return UpdateAccountMedia(
+                    success=False,
+                    message="Profile banner must be a PNG or JPEG file."
+                )
+            user.banner = banner['promise']
+            banner_url = info.context.build_absolute_uri(
+                user.banner.url)
+
+        user.save()
+
+        return UpdateAccountMedia(
+            success=True,
+            message="User media updated successfully.",
+            avatar_url=avatar_url,
+            banner_url=banner_url
+        )
+
+
+class UpdateAccountPassword(graphene.Mutation):
     success = graphene.Boolean(required=True)
     message = graphene.String()
 
@@ -67,74 +165,12 @@ class UpdatePassword(graphene.Mutation):
         user = info.context.user
 
         if not user.check_password(current_password):
-            return UpdatePassword(success=False, message="Current password is incorrect.")
+            return UpdateAccountPassword(success=False, message="Current password is incorrect.")
 
         user.set_password(new_password)
         user.save()
 
-        return UpdatePassword(success=True, message="Account password has been updated.")
-
-
-class UpdateEmail(graphene.Mutation):
-    success = graphene.Boolean(required=True)
-    message = graphene.String()
-
-    class Arguments:
-        new_email = graphene.String(required=True)
-
-    @login_required
-    def mutate(self, info, new_email):
-        user = info.context.user
-
-        if User.objects.filter(email=new_email).exists():
-            return UpdateEmail(success=True, message="An account with that email already exists.")
-
-        user.email = new_email
-        user.save()
-
-        return UpdateEmail(success=True, message="Account email has been updated.")
-
-
-class UpdateProfilePicture(graphene.Mutation):
-    success = graphene.Boolean(required=True)
-    message = graphene.String()
-    profile_picture_url = graphene.String()
-
-    class Arguments:
-        profile_picture = Upload(required=True)
-
-    @login_required
-    def mutate(self, info, profile_picture):
-        user = info.context.user
-
-        allowed_extensions = ['.jpg', '.jpeg', '.png']
-        if not any(profile_picture['name'].endswith(ext) for ext in allowed_extensions):
-            return UpdateProfilePicture(
-                success=False,
-                message="Profile picture must be a PNG or JPEG file.",
-                profile_picture_url=None
-            )
-
-        user.profile_picture = profile_picture['promise']
-        user.save()
-
-        profile_picture_url = info.context.build_absolute_uri(
-            user.profile_picture.url)
-
-        return UpdateProfilePicture(
-            success=True,
-            message="Profile picture updated.",
-            profile_picture_url=profile_picture_url,
-        )
-
-
-class ObtainJSONWebToken(graphql_jwt.JSONWebTokenMutation):
-    refresh_token = graphene.NonNull(graphene.String)
-    user = graphene.NonNull(UserType)
-
-    @classmethod
-    def resolve(cls, root, info, **kwargs):
-        return cls(user=info.context.user, refresh_token=create_refresh_token(info.context.user))
+        return UpdateAccountPassword(success=True, message="Account password has been updated.")
 
 
 class Query(graphene.ObjectType):
@@ -146,14 +182,14 @@ class Query(graphene.ObjectType):
 
 
 class Mutation(graphene.ObjectType):
-    create_user = CreateUser.Field()
-    update_email = UpdateEmail.Field()
-    update_profile_picture = UpdateProfilePicture.Field()
-    update_password = UpdatePassword.Field()
-    token_auth = ObtainJSONWebToken.Field()
+    auth = Auth.Field()
     verify_token = graphql_jwt.Verify.Field()
     revoke_token = graphql_jwt.Revoke.Field()
     refresh_token = graphql_jwt.Refresh.Field()
+    create_account = CreateAccount.Field()
+    update_account_profile = UpdateAccountProfile.Field()
+    update_account_media = UpdateAccountMedia.Field()
+    update_account_password = UpdateAccountPassword.Field()
 
 
 schema = build_schema(query=Query, mutation=Mutation,
