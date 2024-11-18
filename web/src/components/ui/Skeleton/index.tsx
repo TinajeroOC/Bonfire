070895dc78359d7ddcb41841782c3c06a5c730aa
TinajeroOC <88462409+TinajeroOC@users.noreply.github.com>
2024-11-17 23:36:14 -0800

    Add sidebar component

---
+++ web/src/components/ui/Skeleton/index.tsx
@@ -0,0 +1,7 @@
+import { ny } from "@/lib/utils"
+
+function Skeleton({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
+  return <div className={ny("bg-muted animate-pulse rounded-md", className)} {...props} />
+}
+
+export { Skeleton }
